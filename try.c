973e598252592800d04bda6ba3d1d2b5e4f7a733
diiafyra <dii.afyra.work@gmail.com>
2023-10-23 17:05:12 +0700

    changecode.3

--- try.c
+++ try.c
@@ -2,12 +2,16 @@
 
 int main (){
 	printf("Say hi: ");
-	char greeting[100];
-	scanf("%s", greeting);
-	if(strcmp(greeting,"hi") == 0){
-	printf("Annhon, this is trangdi");
-	} else{
-		printf("Type 'hi', please: ");
-	} 
+	int i;
+	while (i < 100){
+		i++;
+		char greeting[100];
+		scanf("%s", greeting);
+		if(strcmp(greeting,"hi") == 0){
+		printf("Annhon, this is trangdi");
+		} else{
+			printf("Type 'hi', please: ");
+		} 
+	}
 	return 0;
 }
